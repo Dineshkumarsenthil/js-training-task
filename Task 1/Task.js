c60ca461e7c32d56bb8e88aa43b7890b3919c82e
Dineshkumarsenthil <dineshkumarsenthilk123@gmail.com>
2026-04-17 15:36:06 +0530

    Added JS training task

---
+++ Task 1/Task.js
@@ -0,0 +1,75 @@
+//Task 1
+var a = 50;
+a = 100;
+console.log(a);
+
+//Task 2
+let marks = 80;
+marks = marks + 10;
+console.log(marks);
+
+//Task 3
+const price = 500;
+const finalPrice = price + 100;
+console.log(finalPrice);
+
+//Task 4
+console.log("Welcome Team");
+console.log(2026);
+console.log(true);
+
+//Task 5
+console.log(typeof("JavaScript"));
+console.log(typeof(250));
+console.log(typeof(false));
+
+//Task 6
+let fruits = ["apple","banana",
+  "mango","grapes","orange"];
+
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[fruits.length-1])
+
+//Task 7
+let student = {
+  name: "Dineshkumar",
+  age: 21,
+  course: "Cloud"
+};
+console.log(student.name);
+console.log(student.age);
+
+//Task 8
+console.log(20 + 10);  
+console.log(50 - 25);  
+console.log(5  * 5);   
+console.log(100 / 4);  
+console.log(20 % 3);   
+
+//Task 9 
+let x = 5;
+x++;
+console.log(x);  
+
+let y = 10;
+y--;
+console.log(y);   
+
+//Task 10
+console.log(10 >  5);       
+console.log(5  <  2);       
+console.log(20 == "20");   
+console.log(15 === "15"); 
+console.log(10 != 8);  
+
+//Task 11
+console.log(5 > 2 && 10 > 3);
+console.log(7 < 5 || 8  > 2);
+console.log(!(10 > 5));
+
+//Task 12
+let age = 18;
+age >= 18
+  ? console.log("Eligible")
+  : console.log("Not Eligible");
